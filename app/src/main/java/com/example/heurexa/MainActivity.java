5de05d7cc4c63f92383cc69bc64e7bf1cd469736
sarswatPreeti <saraswatpreeti.developer@gmail.com>
2024-09-14 23:00:48 +0530

    updated logo

--- app/src/main/java/com/example/heurexa/MainActivity.java
+++ app/src/main/java/com/example/heurexa/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.heurexa;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button btn = (Button)findViewById(R.id.bttn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, getStarted.class));
+            }
         });
     }
 }
