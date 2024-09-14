a25499da8c6fd3a4449aa0e78c8f9c4a40b56db3
abhay <abhayforwork02@gmail.com>
2024-09-14 22:40:06 +0530

    splash screen shanges

--- app/src/main/java/com/example/heurexa/SplashScreen.java
+++ app/src/main/java/com/example/heurexa/SplashScreen.java
@@ -1,6 +1,12 @@
 package com.example.heurexa;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
-
+    ImageView img;
+    TextView Heurexa,smile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,22 @@ public class SplashScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        img = findViewById(R.id.back);
+        Heurexa=findViewById(R.id.Heurexa);
+        smile=findViewById(R.id.smile);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreen.this, MainActivity.class);
+              startActivity(i);
+              finish();
+            }
+        },2000);
+        Animation myanim=AnimationUtils.loadAnimation(SplashScreen.this,R.anim.bg);
+        img.startAnimation(myanim);
+        Animation tet=AnimationUtils.loadAnimation(SplashScreen.this,R.anim.bounce);
+        Heurexa.startAnimation(tet);
+        Animation tet2=AnimationUtils.loadAnimation(SplashScreen.this,R.anim.bounce);
+        smile.startAnimation(tet2);
     }
 }
